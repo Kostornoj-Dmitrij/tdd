07834b8d175689ac6b92df3fd10c9e3b20fe5395
Kostornoj-Dmitrij <D1.imosa@yandex.ru>
2024-11-13 18:34:53 +0500

    first part of homework

---
+++ cs/TagsCloudVisualization/CloudLayouter/CircularCloudLayouter.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+using TagsCloudVisualization.Interfaces;
+
+namespace TagsCloudVisualization.CloudLayouter
+{
+    public class CircularCloudLayouter : ICloudLayouter
+    {
+        private readonly Point Center;
+        private List<Rectangle> rectangles;
+        private double angle;
+        private const double SpiralStep = 0.1;
+        public List<Rectangle> Rectangles => rectangles;
+        public double Angle => angle;
+
+        public CircularCloudLayouter(Point center)
+        {
+            if (center.X < 0 || center.Y < 0)
+                throw new ArgumentException("Center point coordinates cannot be negative.", nameof(center));
+            
+            Center = center;
+            rectangles = new List<Rectangle>();
+            angle = 0;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            if (rectangleSize.Width <= 0 || rectangleSize.Height <= 0)
+                throw new ArgumentException("Size should be positive value", nameof(rectangleSize));
+
+            Rectangle newRectangle;
+            do
+            {
+                var location = GetNextPointOnSpiral();
+                var rectangleLocation = new Point(location.X - rectangleSize.Width / 2, 
+                                                location.Y - rectangleSize.Height / 2);
+                newRectangle = new Rectangle(rectangleLocation, rectangleSize);
+            } while (IsIntersects(newRectangle));
+
+            Rectangles.Add(newRectangle);
+            return newRectangle;
+        }
+
+        private Point GetNextPointOnSpiral()
+        {
+            var x = (int)(Center.X + Angle * Math.Cos(Angle));
+            var y = (int)(Center.Y + Angle * Math.Sin(Angle));
+            angle += SpiralStep;
+            return new Point(x, y);
+        }
+
+        private bool IsIntersects(Rectangle rectangle)
+        {
+            foreach (var existingrectangle in Rectangles)
+            {
+                if (existingrectangle.IntersectsWith(rectangle))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Tests/CircularCloudLayouter_should.cs
@@ -0,0 +1,145 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using System.Drawing;
+using TagsCloudVisualization.CloudLayouter;
+
+namespace TagsCloudVisualization.Tests
+{
+    [TestFixture]
+    public class CircularCloudLayouter_should
+    {
+        private CircularCloudLayouter layouter;
+        private Point center;
+        private Size[] rectangleSizes = new[]
+        {
+            new Size(10, 10),
+            new Size(20, 20),
+            new Size(30, 10),
+            new Size(15, 25),
+            new Size(10, 10)
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            center = new Point(0, 0);
+            layouter = new CircularCloudLayouter(center);
+        }
+
+        [Test]
+        public void CircularCloud_ShouldBeEmpty_WhenCreated()
+        {
+            layouter.Rectangles.Should().BeEmpty();
+        }
+
+        [TestCase(-1, -1)]
+        [TestCase(-1, 10)]
+        [TestCase(10, -1)]
+        public void CircularCloud_ShouldThrowArgumentException_WhenInvalidCenterCoordinates(int width, int height)
+        {
+            var func = () => new CircularCloudLayouter(new Point(width, height));
+            func.Should().Throw<ArgumentException>();
+        }
+
+        [TestCase(0, 10)]
+        [TestCase(10, 0)]
+        [TestCase(-1, 10)]
+        [TestCase(10, -1)]
+        [TestCase(0, 0)]
+        [TestCase(-10, -10)]
+        public void CircularCloud_ShouldThrowArgumentException_WhenInvalidRectangleSize(int width, int height)
+        {
+            var func = () => layouter.PutNextRectangle(new Size(width, height));
+            func.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldReturnRectangleWithSameSize()
+        {
+            var rectangleSize = new Size(20, 30);
+            var rectangle = layouter.PutNextRectangle(rectangleSize);
+            
+            rectangle.Size.Should().BeEquivalentTo(rectangleSize);
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldPlaceRectangleInCenter_WhenFirstRectangle()
+        {
+            var rectangleSize = new Size(10, 10);
+            var expectedLocation = new Point(-rectangleSize.Width / 2, -rectangleSize.Height / 2);
+            var rectangle = layouter.PutNextRectangle(rectangleSize);
+            
+            rectangle.Location.Should().BeEquivalentTo(expectedLocation);
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldNotIntersectWithPreviousRectangles()
+        {
+            foreach (var size in rectangleSizes)
+            {
+                layouter.PutNextRectangle(size);
+            }
+
+            var rectangles = layouter.Rectangles.ToList();
+
+            for (int i = 0; i < rectangles.Count; i++)
+            {
+                for (int j = i + 1; j < rectangles.Count; j++)
+                {
+                    rectangles[i].IntersectsWith(rectangles[j]).Should().BeFalse();
+                }
+            }
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldNotIntersect_WhenPlacingIdenticalRectangles()
+        {
+            var rectangleSize = new Size(10, 10);
+            var numberOfRectangles = 10;
+
+            for (int i = 0; i < numberOfRectangles; i++)
+            {
+                layouter.PutNextRectangle(rectangleSize);
+            }
+
+            var rectangles = layouter.Rectangles.ToList();
+
+            for (int i = 0; i < rectangles.Count; i++)
+            {
+                for (int j = i + 1; j < rectangles.Count; j++)
+                {
+                    rectangles[i].IntersectsWith(rectangles[j]).Should().BeFalse();
+                }
+            }
+        }
+
+        [Test]
+        public void CircularCloud_ShouldHaveAllPlacedRectangles()
+        {
+            foreach (var size in rectangleSizes)
+            {
+                layouter.PutNextRectangle(size);
+            }
+
+            layouter.Rectangles.Should().HaveCount(rectangleSizes.Length);
+        }
+
+        [Test]
+        public void CircularCloudRectangles_ShouldBeCloseToCenter()
+        {
+            foreach (var size in rectangleSizes)
+            {
+                layouter.PutNextRectangle(size);
+            }
+
+            var rectangles = layouter.Rectangles.ToList();
+
+            foreach (var rectangle in rectangles)
+            {
+                var distanceToCenter = Math.Sqrt(Math.Pow(rectangle.X + rectangle.Width / 2 - center.X, 2) +
+                                                  Math.Pow(rectangle.Y + rectangle.Height / 2 - center.Y, 2));
+                distanceToCenter.Should().BeLessThan(20);
+            }
+        }
+    }
+}
